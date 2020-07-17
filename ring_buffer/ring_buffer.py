e8643deefc13542db526b198214edcc51a53b38d
Tim Shaker <tim@tmshkr.com>
2020-07-17 08:20:15 -0700

    complete ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.index] = item
+            if self.index < self.capacity - 1:
+                self.index += 1
+            else:
+                self.index = 0
 
     def get(self):
-        pass
+        return self.storage
+
+
+buffer = RingBuffer(5)
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+buffer.append('f')
+buffer.append('g')
+buffer.append('h')
+buffer.append('i')
